91f145c1aa424ab8524b26bb9902e337f91f6340
recovery12 <44424630+recovery12@users.noreply.github.com>
2019-10-13 08:01:07 +0530

    Created main.c
    This is a file that contains main.

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/********* DEFINED CONSTANTS *********/
+#define   MAX        20
+
+/********* MAIN STARTS HERE *********/
+int main(void)
+{
+   int        a, b;
+   
+   exit(0);
+}
